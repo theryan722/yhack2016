9b036cafa9d97155db8bdd74cf04668072f7e697
Ryan O'Day <romanguy722@gmail.com>
2016-11-12 11:00:52 -0500

    Update getdistance.php

--- api/getdistance.php
+++ api/getdistance.php
@@ -1,1 +1,23 @@
-
+<?php
+$a1 = $_GET['address1'];
+$a2 = $_GET['address2'];
+echo getDistance($a1, $a2);
+function getDistance($addressFrom, $addressTo){
+    $formattedAddrFrom = str_replace(' ','+',$addressFrom);
+    $formattedAddrTo = str_replace(' ','+',$addressTo);
+    $geocodeFrom = file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.$formattedAddrFrom.'&sensor=false');
+    $outputFrom = json_decode($geocodeFrom);
+    $geocodeTo = file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.$formattedAddrTo.'&sensor=false');
+    $outputTo = json_decode($geocodeTo);
+    $latitudeFrom = $outputFrom->results[0]->geometry->location->lat;
+    $longitudeFrom = $outputFrom->results[0]->geometry->location->lng;
+    $latitudeTo = $outputTo->results[0]->geometry->location->lat;
+    $longitudeTo = $outputTo->results[0]->geometry->location->lng;
+    $theta = $longitudeFrom - $longitudeTo;
+    $dist = sin(deg2rad($latitudeFrom)) * sin(deg2rad($latitudeTo)) +  cos(deg2rad($latitudeFrom)) * cos(deg2rad($latitudeTo)) * cos(deg2rad($theta));
+    $dist = acos($dist);
+    $dist = rad2deg($dist);
+    $miles = $dist * 60 * 1.1515;
+    return $miles;
+}
+?>
