16df6dae130d033c2ab64aaf4c4fd3b76d2492b2
Ryan O'Day <romanguy722@gmail.com>
2016-11-12 14:49:34 -0500

    Fix Balance

--- api/balance.php
+++ api/balance.php
@@ -1,39 +1,29 @@
 <?php
         require 'config.php';
-
         $method = $_GET["action"];
         $master_id = "master@master.com";
-
         // Open database
         $db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-
         if (!$db)
                 die("Connection failed: " . mysqli_connect_error());
-
         // Always get current balance amount
         $query = "SELECT balance FROM profiles WHERE email='{$master_id}'";
         $result = mysqli_query($db, $query);
-
         //Should only be one row
         $row = mysqli_fetch_assoc($result);
         $current_balance = $row["balance"];
-
         if (strcmp("get", $method) == 0) {
                 echo $current_balance;
-
         } else {
                 // Get amount specified in URL and add/subtract from balance
                 $amount = intval($_GET["amount"]);
-
-                $current_balance += strcmp("add"), $method) ? -$amount : $amount;
-
-                $newquery = "UPDATE profiles SET balance=$current_balance WHERE email='{$master_id}'";
-                if (mysqli_query($db, $newquery))
-                        echo "Record updated successfully";
-                else
-                        echo "Error updating record: " . mysqli_error($db);
+                $current_balance += strcmp("add", $method) ? -$amount : $amount;
+                $newquery = "UPDATE profiles SET balance='$current_balance' WHERE email='{$master_id}'";
+                if (mysqli_query($db, $newquery)){
+                        echo "Success";
+               } else {
+                        echo "Fail";
+               }
         }
-
         mysqli_close($db);
-
  ?>
