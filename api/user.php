4a452c32947db86560d4eac0dadc935418b8f5df
Ryan O'Day <romanguy722@gmail.com>
2016-11-13 02:29:57 -0500

    Fix stuff and add verification

--- api/user.php
+++ api/user.php
@@ -1,16 +1,16 @@
 <?php
 	require 'config.php';
 	// both requests have action and email
-	$action = $_POST["action"];
-	$email = $_POST["email"];
+	$action = $_GET["action"];
+	$email = $_GET["email"];
 	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 	if (strcmp($action, "add") == 0) {
-		$password = $_POST["password"];
-		$name = $_POST["name"];
-		$address = $_POST["address"];
-		$balance = intval($_POST["balance"]);
-		$budget = intval($_POST["budget"]);
-		$radius	 = intval($_POST["radius"]);
+		$password = $_GET["password"];
+		$name = $_GET["name"];
+		$address = $_GET["address"];
+		$balance = intval($_GET["balance"]);
+		$budget = intval($_GET["budget"]);
+		$radius	 = intval($_GET["radius"]);
 		$sql = "INSERT INTO profiles (email, password, name, address, balance, budget, radius) VALUES ('$email', '$password', '$name', '$address', '$balance', '$budget', '$radius')";
     if (mysqli_query($conn, $sql)) {
 			echo "Success";
@@ -18,11 +18,25 @@
 			echo "Fail";
 		}
 	} else if (strcmp($action, "get") == 0) {
-		$query = "SELECT * FROM $profiles WHERE email = $email";
+		$query = "SELECT * FROM profiles WHERE email = '$email'";
 		$result = mysqli_query($conn, $query);
 		$array = mysqli_fetch_assoc($result);
 		$json = json_encode($array);
 		echo $json;
+	} else if (strcmp($action, "verify") == 0) {
+	  $success = false;
+	  $pass = $_GET["password"];
+    $sql = "SELECT * FROM profiles WHERE email='$email' AND password='$pass'";
+		$success = false;
+		$result = mysqli_query($conn, $sql);
+    while($row = mysqli_fetch_array($result)) {
+        $success = true;
+    }
+    if($success == true) {
+        echo 'Success';
+    } else {
+        echo 'Fail';
+    }
 	}
 	mysqli_close($conn);
 ?>
